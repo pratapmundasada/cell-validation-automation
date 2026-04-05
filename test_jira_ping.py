0e2c9a6f1876995d6717c385d806f1898b258c20
pratapmundasada <pratap.mundasada@revo-tech.co.uk>
2026-04-05 20:42:01 +0100

    Update test_jira_ping.py

--- test_jira_ping.py
+++ test_jira_ping.py
@@ -1,18 +1,22 @@
+import os
 from jira import JIRA
 
-# 1. Hardcode for a 1-time local test
-JIRA_SERVER = https://revo-tech.atlassian.net/
-JIRA_EMAIL =  pratap.mundasada@revo-tech.co.uk
-JIRA_API_TOKEN = ATATT3xFfGF0nNdopLS2-LiiykzJ3kHRyiJDWC1UddiCq7YZDSp4DK2qoUcskfpZfbcYIUa3X3R7cEgvHmoYWZjRHY7aVGPrHLiVzC513ErUHIC3FZDnb7VnHVx__wRXZC6CGN_QScEuCtWhpyWF1Y-6d0eXpaY7L2ZaQL8UrbooZ6p6vAkiCEs=FA028563
+def connect():
+    # os.getenv searches the environment variables of the GitHub Runner
+    server = os.getenv("JIRA_SERVER")
+    email = os.getenv("JIRA_EMAIL")
+    token = os.getenv("JIRA_API_TOKEN")
 
+    # Principal Check: Ensure variables actually arrived
+    if not server:
+        print("❌ Error: JIRA_SERVER variable is empty. Check your YAML!")
+        return
 
-def check():
     try:
-        # Use the variables defined above
-        j = JIRA(server=JIRA_SERVER, basic_auth=(JIRA_EMAIL, JIRA_API_TOKEN))
-        print(f"✅ Connected to: {j.server_info()['serverTitle']}")
+        jira = JIRA(server=server, basic_auth=(email, token))
+        print(f"✅ Success! Connected to: {jira.server_info()['serverTitle']}")
     except Exception as e:
         print(f"❌ Connection Failed: {e}")
 
 if __name__ == "__main__":
-    check()
+    connect()
